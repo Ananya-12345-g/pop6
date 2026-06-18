c44935c4021895556cce775a6b0349afadbf3e4f
CrazyAthma <kirangrgm@gmail.com>
2026-06-18 09:09:30 +0530

    first commit

---
+++ asd/src/test/java/qwer/asd/AppTest.java
@@ -0,0 +1,19 @@
+package qwer.asd;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
